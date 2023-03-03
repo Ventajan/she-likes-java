31169486cc9f1e5f4a4c08ec60f27bf4effa9b92
Vilma <vilmjank@yahoo.com>
2023-03-03 19:06:54 +0000

    int data type

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("10000000");
+        System.out.println("10005000");
+
+// holder for data
+// needs good name e.g. myDaughterAge
+// needs type of variable
+// that data type limit possible set of allowed values
+// int - integer numbers
+    int myDaughterAge = 5;
+       myDaughterAge = 26;
+        System.out.println(myDaughterAge);
+
+
     }
 }
