6c6b4750752c57ad58d4199d8e07e8e968bb871f
Vilma <vilmjank@yahoo.com>
2023-03-08 16:43:53 +0200

    calculator from 3 suported operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,24 +8,48 @@ public class Calculator {
 
 
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//        System.out.println("Calculator....");
+//        System.out.println("Calculating sum of two numbers");
+//        System.out.println("Provide first number");
+//        int firstNumber;
+//        int secondNumber;
+////        TODO: explain object creation
+//        Scanner inputReader = new Scanner(System.in);
+//        firstNumber = inputReader.nextInt();
+//        System.out.println("You provided number: " + firstNumber);
+//        System.out.println("Provide second number");
+//
+//        secondNumber = inputReader.nextInt();
+//        System.out.println("You provided number: " + secondNumber);
+//        int sum = firstNumber + secondNumber;
+//        System.out.println("Sum is: " + sum);
+//        Exercice 8
+//        Create calculator with 4 suported operations which is able to read from user
+
+        public static void main(String[] args) {
         System.out.println("Calculator....");
-        System.out.println("Calculating sum of two numbers");
+        System.out.println("Calculating sum of four numbers");
         System.out.println("Provide first number");
         int firstNumber;
         int secondNumber;
+        int thirdNumber;
+
 //        TODO: explain object creation
         Scanner inputReader = new Scanner(System.in);
         firstNumber = inputReader.nextInt();
         System.out.println("You provided number: " + firstNumber);
         System.out.println("Provide second number");
-
         secondNumber = inputReader.nextInt();
         System.out.println("You provided number: " + secondNumber);
-        int sum = firstNumber + secondNumber;
+            System.out.println("Provide third number");
+        thirdNumber = inputReader.nextInt();
+        System.out.println("You provided number: " + thirdNumber);
+
+
+
+        int sum = firstNumber + secondNumber+thirdNumber;
         System.out.println("Sum is: " + sum);
-
-
 
 
 
