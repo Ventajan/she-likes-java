2021849f6d77eb38f20b1e7e8cea51f38b42b56f
Vilma <vilmjank@yahoo.com>
2023-03-12 11:49:06 +0200

    BouncerDiscoVersion 3

--- src/com/sda/she_likes_java/homework/BouncerInDiscoVersion3.java
+++ src/com/sda/she_likes_java/homework/BouncerInDiscoVersion3.java
@@ -2,9 +2,9 @@ package com.sda.she_likes_java.homework;
 
 import java.util.Scanner;
 
-public class BouncerInDiscoVersion3 {
-    public static void main(String[]args) {
+public class BounderInDiscoVersion3 {
 
+    public static void main(String[] args) {
         System.out.println("Party time???");
 
         System.out.println("Somebody is coming to club");
@@ -13,21 +13,16 @@ public class BouncerInDiscoVersion3 {
         Scanner inputReader = new Scanner(System.in);
         String declineMessage = "Sorry, go home:)";
         int age = inputReader.nextInt();
-        System.out.println("Let me know your height now");
+        System.out.println("Let me know Your height now: ");
         int height = inputReader.nextInt();
-        boolean isAlloudToPartyAge = age >= 18 &&height>=200;
-//
-        boolean isAlloudToPartyage;
-        if (isAlloudToPartyage ) {
-            System.out.println("HAve a grate time at party t:)");
+        boolean isAllowedToParty = age >= 18 && height >= 200;
 
-            } else {
-                System.out.println(declineMessage);
-            }
-
+        if (isAllowedToParty) {
+            System.out.println("Have a great time at party!!!");
+        } else {
+            System.out.println(declineMessage);
         }
-
     }
-
+}
 
 
