6aa4cf24bef48e3d9459a229d9f1a9b8f75d65da
Vilma <vilmjank@yahoo.com>
2023-03-12 19:46:23 +0200

    negation operator explained

--- src/com/sda/she_likes_java/operators/Operators.java
+++ src/com/sda/she_likes_java/operators/Operators.java
@@ -1,53 +1,54 @@
 package com.sda.she_likes_java.operators;
 
 public class Operators {
-    public static void main(String[] args) {
-        System.out.println("Now, let's try some operators");
-        System.out.println("Logical operators");
 
-        boolean firstValue;
-        boolean secondValue;
-        boolean evaluationResult;
-        System.out.println("&& operator");
-        firstValue = true;
-        secondValue = true;
-        evaluationResult = firstValue && secondValue;
-        System.out.println("true && true: " + evaluationResult);
+        public static void main(String[] args) {
+            System.out.println("Now, let's try some operators");
+            System.out.println("Logical operators");
 
-        firstValue = true;
-        secondValue = false;
-        evaluationResult = firstValue && secondValue;
-        System.out.println("true && false: " + evaluationResult);
+            boolean firstValue;
+            boolean secondValue;
+            boolean evaluationResult;
+            System.out.println("&& operator");
+            firstValue = true;
+            secondValue = true;
+            evaluationResult = firstValue && secondValue;
+            System.out.println("true && true: " + evaluationResult);
 
-        firstValue = false;
-        secondValue = true;
-        evaluationResult = firstValue && secondValue;
-        System.out.println("false && true: " + evaluationResult);
+            firstValue = true;
+            secondValue = false;
+            evaluationResult = firstValue && secondValue;
+            System.out.println("true && false: " + evaluationResult);
 
-        firstValue = false;
-        secondValue = false;
-        evaluationResult = firstValue && secondValue;
-        System.out.println("false && false: " + evaluationResult);
+            firstValue = false;
+            secondValue = true;
+            evaluationResult = firstValue && secondValue;
+            System.out.println("false && true: " + evaluationResult);
 
-        System.out.println();
-        System.out.println("Let's test negation operator");
-        firstValue = false;
-        secondValue = !firstValue;
-        System.out.println("!false is: " + secondValue);
+            firstValue = false;
+            secondValue = false;
+            evaluationResult = firstValue && secondValue;
+            System.out.println("false && false: " + evaluationResult);
 
-        firstValue = true;
-        secondValue = !firstValue;
-        System.out.println("!true is: " + secondValue);
+            System.out.println();
+            System.out.println("Let's test negation operator");
+            firstValue = false;
+            secondValue = !firstValue;
+            System.out.println("!false is: " + secondValue);
 
-        boolean polite = false;
-        if (!polite) {
-            System.out.println("You're not polite, or just naughty");
+            firstValue = true;
+            secondValue = !firstValue;
+            System.out.println("!true is: " + secondValue);
+
+            // negation of isPolite equals naughty
+            boolean isPolite = false;
+            if (!isPolite) {
+                System.out.println("You're not polite, or just naughty");
+            }
+
+            boolean naughty = true;
+            if (naughty) {
+                System.out.println("You're not polite, or just naughty");
+            }
         }
-
-        boolean naughty = true;
-        if (naughty) {
-            System.out.println("You're not polite, or just naughty");
-        }
-
     }
-}
