79a360247aedaabed24cecf9050b4ff11966ac58
Vilma <vilmjank@yahoo.com>
2023-03-13 19:45:54 +0200

    lwhile loop second example

---
+++ src/com/sda/she_likes_java/loops/while_loop/WhileGrow.java
@@ -0,0 +1,17 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class WhileGrow {
+    public static void main(String[] args) {
+
+        int ageOfTheKid = 1;
+
+        while (ageOfTheKid <= 18) {
+            System.out.println("Mommy can I go to disco??");
+            System.out.println("No");
+
+//            ageOfTheKid = ageOfTheKid + 1;
+//            ageOfTheKid++; this is shorter version of: ageOfTheKid =ageOfTheKid +1;
+        }
+
+    }
+}
---
+++ src/com/sda/she_likes_java/loops/while_loop/WhileLoop.java
@@ -0,0 +1,4 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class WhileLoop {
+}
