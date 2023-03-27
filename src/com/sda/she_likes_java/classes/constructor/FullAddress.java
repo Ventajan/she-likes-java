853b4272a5fbdfdd04a02aa0104e98fff2e68129
Vilma <vilmjank@yahoo.com>
2023-03-27 20:39:22 +0300

    experimenats with class contstructor

---
+++ src/com/sda/she_likes_java/classes/constructor/FullAddress.java
@@ -0,0 +1,47 @@
+package com.sda.she_likes_java.classes.constructor;
+
+
+public class FullAddress {
+    public String country;
+    public String city;
+    public String street;
+    // 02-987
+    // 011
+    public String postalCode;
+
+    //constructor
+    public FullAddress(String country, String city, String street, String postalCode) {
+        this.country = country;
+        this.city = city;
+        this.street = street;
+        this.postalCode = postalCode;
+    }
+
+    //default constructor mean without any parameters
+    public FullAddress() {
+    }
+
+    @Override
+    public String toString() {
+        return "FullAddress{" +
+                "country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        // full address
+        FullAddress myAddress = new FullAddress();
+        myAddress.city = "Warsaw";
+        myAddress.country = "Italy";
+        myAddress.street = "La Strada";
+        myAddress.postalCode = "01-001";
+        System.out.println("fullAddress created by hand: " + myAddress);
+        FullAddress createdWithFullConstructor = new FullAddress("Spain", "Barselona", "La", "12345");
+
+        System.out.println("FullAddress created with constructor: " + createdWithFullConstructor);
+    }
+}
+
