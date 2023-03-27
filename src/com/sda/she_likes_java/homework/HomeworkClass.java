1b98d4f79398a8ad010666013dee27b71844af24
Vilma <vilmjank@yahoo.com>
2023-03-27 19:16:14 +0300

    StringExample example

---
+++ src/com/sda/she_likes_java/homework/HomeworkClass.java
@@ -0,0 +1,35 @@
+package com.sda.she_likes_java.homework;
+
+public class HomeworkClass {
+//    Exercise 15.
+//    Create a Parent class containing the following variables:
+//            - first name
+//- surname
+//- address - (use the class Address)
+//            - firstChild (use class Person)
+//- secondChild (use class Person)
+
+    public class Parent {
+        String firstname;
+        String surname;
+        Person firstChild;
+        Person secondChild;
+        Address address;
+
+        public class Address {
+            Address  = new Address;
+        }
+
+        public static void main(String[] args) {
+            Person firstChild = new Person;
+        }
+
+        public static void main(String[] args) {
+            Person secondChild = new Person;
+        }
+
+
+        public class Person;
+    }
+}
+
