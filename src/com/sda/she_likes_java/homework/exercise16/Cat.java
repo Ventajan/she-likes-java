ba6b68f99ad5afcb25b65698404a01375f763167
Vilma <vilmjank@yahoo.com>
2023-03-31 19:15:34 +0300

    cat

---
+++ src/com/sda/she_likes_java/homework/exercise16/Cat.java
@@ -0,0 +1,33 @@
+package com.sda.she_likes_java.homework.exercise16;
+
+public class Cat {
+    public Cat() {
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{}";
+
+    }
+}
+
