6488c3222dd83675f63792ded857adbca5f8e733
Vilma <vilmjank@yahoo.com>
2023-03-31 13:40:47 +0300

    while loope exercize with 10 green bottles

---
+++ src/com/sda/she_likes_java/homework/CodingBook/BottleSong.java
@@ -0,0 +1,22 @@
+package com.sda.she_likes_java.homework.CodingBook;
+
+public class BottleSong {
+    public static void main(String[] args) {
+        int bottlesNum = 10;
+        String word = "bottles";
+        while (bottlesNum >0){
+            if (bottlesNum==1) {
+                word = "bottle";
+            }
+            System.out.println(bottlesNum + "green" + word + ",hanging on the wall");
+            System.out.println(bottlesNum + "green" + word + ",hanging on the wall");
+            System.out.println("And if one green bottle should accindentally fall,");
+            bottlesNum = bottlesNum - 1;
+            if (bottlesNum>0){
+                System.out.println("There will be" + bottlesNum + "green" + word + ",hanging on the wall");
+            } else {
+                System.out.println ("There will be no green bottles, hanging on the wall");
+            }
+        }
+    }
+}
