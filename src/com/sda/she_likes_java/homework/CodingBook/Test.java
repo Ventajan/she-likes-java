f0ed5b8dd4d11bdb204f680e32566e1ae238ca69
Vilma <vilmjank@yahoo.com>
2023-04-03 15:45:01 +0300

    short exercise to learn while if

---
+++ src/com/sda/she_likes_java/homework/CodingBook/Test.java
@@ -0,0 +1,9 @@
+package com.sda.she_likes_java.homework.CodingBook;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x<5) {
+    }
+}
