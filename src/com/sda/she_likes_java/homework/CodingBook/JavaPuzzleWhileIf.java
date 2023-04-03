7adba6dd6aff7b83be57b2ea44fe15af9a33901c
Vilma <vilmjank@yahoo.com>
2023-04-03 19:36:11 +0300

    short exercise to learn while if

---
+++ src/com/sda/she_likes_java/homework/CodingBook/JavaPuzzleWhileIf.java
@@ -0,0 +1,14 @@
+//package com.sda.she_likes_java.homework.CodingBook;
+//
+//public class JavaPuzzleWhileIf {
+//    public static void main(String[] args) {
+//        int x = 0;
+//        while ( x>4) {
+//            System.out.print("%java PoolPuzzleOne");
+//        }
+//        if (x<1) {
+//            System.out.println("a");
+//        }
+//        x=x+
+//    }
+//}
