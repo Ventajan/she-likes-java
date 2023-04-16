698fd8bd0b8f23917cf8bceb9b78cc91ac5fd487
Vilma <vilmjank@yahoo.com>
2023-04-16 11:55:56 +0300

    object references

---
+++ src/com/sda/she_likes_java/homework/CodingBook/Dog.java
@@ -0,0 +1,18 @@
+package com.sda.she_likes_java.homework.CodingBook;
+
+public class Dog {
+    String name;
+
+
+       public void  bark() {
+           System.out.println(name + "says Ruff");
+       }
+       public void eat () {
+
+       }
+       public void chaseCAt () {
+
+       }
+
+
+}
