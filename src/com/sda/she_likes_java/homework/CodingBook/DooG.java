3c5e9c941caaa9786300d35db5904f8ad58fc69e
Vilma <vilmjank@yahoo.com>
2023-04-16 18:06:36 +0300

    objects behavior

---
+++ src/com/sda/she_likes_java/homework/CodingBook/DooG.java
@@ -0,0 +1,15 @@
+package com.sda.she_likes_java.homework.CodingBook;
+
+public class DooG {
+    int size;
+    String name;
+    void  bark() {
+        if (size >60){
+            System.out.println("Woff! Woff!");
+        } else if ( size >8){
+            System.out.println("Riuff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
---
+++ src/com/sda/she_likes_java/homework/CodingBook/DooGTEst.java
@@ -0,0 +1,15 @@
+package com.sda.she_likes_java.homework.CodingBook;
+
+public class DooGTEst {
+    public static void main(String[] args) {
+        DooG one = new DooG();
+        one.size = 70;
+        DooG two = new DooG();
+        two.size = 8;
+        DooG three = new DooG();
+        three.size = 35;
+        one.bark();
+        two.bark();
+        three.bark();
+    }
+}
