7c98248d6d81f05637e438b86a37f945d5ebd596
Vilma <vilmjank@yahoo.com>
2023-05-08 19:39:34 +0300

    generic way

---
+++ src/com/sda/she_likes_java/homework/exercise22/ShoppingCart.java
@@ -0,0 +1,31 @@
+package com.sda.she_likes_java.homework.exercise22;
+import java.util.ArrayList;
+import java.util.List;
+public class ShoppingCart {
+
+
+
+
+        private List<Item> items;
+
+        public ShoppingCart() {
+            this.items = new ArrayList<>();
+        }
+
+        public void addProductToBasket(Item product) {
+            items.add(product);
+        }
+
+        public void displayProducts() {
+            System.out.println("Items in shopping cart: " + items);
+        }
+
+        public double calculateTotalBasketValue() {
+            double sum = 0;
+            for (int i = 0; i < items.size(); i++) {
+                sum += items.get(i).getPriceInUSD();
+            }
+
+            return sum;
+        }
+    }
