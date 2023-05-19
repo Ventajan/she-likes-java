7c64b5b55dc9c5beb8bf15d0ac9a7478cb915c09
Vilma <vilmjank@yahoo.com>
2023-05-19 19:15:22 +0300

    Enums homework

--- src/com/sda/she_likes_java/TimeAndDate/TimeAndDate.java
+++ src/com/sda/she_likes_java/TimeAndDate/TimeAndDate.java
@@ -4,23 +4,33 @@ package com.sda.she_likes_java.TimeAndDate;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
-public class TimeAndDate{
+public class TimeAndDate {
 
-        public static void main(String[] args) {
-            LocalDate currentDate = LocalDate.now();
-            System.out.println("Current date is: " + currentDate);
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Current date is: " + currentDate);
 
-            System.out.println("Current year is: " + currentDate.getYear());
-            System.out.println("Current month is: " + currentDate.getMonth());
-            System.out.println("Current day of the month is: " + currentDate.getDayOfMonth());
+        System.out.println("Current year is: " + currentDate.getYear());
+        System.out.println("Current month is: " + currentDate.getMonth());
+        System.out.println("Current day of the month is: " + currentDate.getDayOfMonth());
 
-            LocalDateTime currentDateAndTime = LocalDateTime.now();
-            System.out.println("Current date and time in my location is: " + currentDateAndTime);
+        LocalDateTime currentDateAndTime = LocalDateTime.now();
+        System.out.println("Current date and time in my location is: " + currentDateAndTime);
 
-            System.out.println("Let's format a little bit our date and time");
+        System.out.println("Let's format a little bit our date and time");
 //        System.out.println(currentDateAndTime.format(DateTimeFormatter.RFC_1123_DATE_TIME));
-            System.out.println(currentDateAndTime.format(DateTimeFormatter.BASIC_ISO_DATE));
-        }
+        System.out.println(currentDateAndTime.format(DateTimeFormatter.BASIC_ISO_DATE));
+
+        LocalDate now = LocalDate.now();
+        LocalDate future = now.plus(5, ChronoUnit.DAYS);
+        System.out.println("now is: " + now);
+        System.out.println("future is: " + future);
+        LocalDate previousYear = now.minusYears(1);
+        System.out.println("previous year:" + previousYear);
+        boolean isFuture = previousYear.isBefore(now);
+
     }
+}
 
