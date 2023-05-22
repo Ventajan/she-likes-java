52941218826375da35d615f3203ff54b58eb339c
Vilma <vilmjank@yahoo.com>
2023-05-22 21:19:06 +0300

    Examplesmwith custom exceptions

---
+++ src/com/sda/she_likes_java/exceptions/MyBrokenException.java
@@ -0,0 +1,22 @@
+package com.sda.she_likes_java.exceptions;
+
+public class MyBrokenException extends Throwable {
+
+
+    private String message;
+
+    public MyBrokenException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "MyBrokenException{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
