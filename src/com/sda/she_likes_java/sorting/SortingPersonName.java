c820c26b6eb45ee12c8fba8125a70dc9e5439f1c
Vilma <vilmjank@yahoo.com>
2023-06-05 21:11:00 +0300

    Lambdas

--- src/com/sda/she_likes_java/sorting/SortingPersonName.java
+++ src/com/sda/she_likes_java/sorting/SortingPersonName.java
@@ -11,23 +11,24 @@ public class SortingPersonName {
         SortedPerson olaf = new SortedPerson("Olaf", 22);
         SortedPerson gustaw = new SortedPerson("Gustaw", 11);
         SortedPerson zorro = new SortedPerson("Zorro", 11);
-        SortedPerson zorro1 = new SortedPerson("Zorro1", 7);
+        SortedPerson zorro1 = new SortedPerson("Zorro", 7);
         SortedPerson zorra = new SortedPerson("Zorra", 11);
         List<SortedPerson> persons = new ArrayList<>();
         persons.add(olaf);
         persons.add(gustaw);
         persons.add(zorro);
         persons.add(zorro);
+        persons.add(zorro);
+        persons.add(zorra);
         persons.add(zorro1);
 
         Collections.sort(persons);
-        System.out.println("After sorting:" + persons);
+        System.out.println("After sorting: " + persons);
 
-
-// if returns -1 that means: olaf, gustaw;
-//        if returns 1 that means: gustaf, olaf
-//        System.out.println("olaf.compareTo(gustaw):"+olaf.compareTo(gustaw));
-//        System.out.println("gustaw vs zorro:"+gustaw.compareTo(zorro));
+        // if returns -1 that means: olaf, gustaw
+        // if returns 1 that means: gustaw, olaf
+//        System.out.println("olaf.compareTo(gustaw): " + olaf.compareTo(gustaw));
+//        System.out.println("gustaw vs zorro: " + gustaw.compareTo(zorro));
         Comparator<SortedPerson> reversedAgeOrder = new AgedReversedOrderOfSortedPersons();
         Collections.sort(persons, reversedAgeOrder);
         System.out.println("Reversed order based on age: " + persons);
@@ -38,15 +39,32 @@ public class SortingPersonName {
         names.add("Alma");
         names.add("Karolina");
         Collections.sort(names);
+        System.out.println("sorted names: " + names);
 
-        Object sorted;
-        System.out.println("sorted names:”+" + names);
         Collections.sort(persons, new LexicalOrderForSortedPerson());
-        System.out.println("persons sorted by names:" + persons);
+        System.out.println("persons sorted by name: " + persons);
+
         Collections.sort(persons, new ReversedLexicalOrderForSortedPerson());
-        System.out.println("Persons sorted by name - reversed:" + persons);
+        System.out.println("persons sorted by name - reversed: " + persons);
 
-        Collections.sort(persons, new LexicalOrderForSortedPerson().reversed().thenComparing(reversedAgeOrder));
+        Collections.sort(persons,
+                new LexicalOrderForSortedPerson().reversed()
+                        .thenComparing(reversedAgeOrder));
+
+        Comparator<SortedPerson> reversedAgeOrderLambda = (o1, o2) -> {
+//        return o2.getAge() - o1.getAge();
+            System.out.println("Using my AgedReversedOrderOfSortedPersons class");
+            if (o1.getAge() < o2.getAge()) {
+                return 1;
+            } else if (o1.getAge() > o2.getAge()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        };
+
+        // logically broken
+        // You can provide lambda for Comparable, but it's not real functional interface
+        Comparable<SortedPerson> brokenComparableLambda = o -> 1;
     }
-
 }
