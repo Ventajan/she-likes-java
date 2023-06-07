3080e65781c31df2afad0cb625b847859d8fb970
Vilma <vilmjank@yahoo.com>
2023-06-07 20:52:34 +0300

    registerNEwUser mthod implemented

--- src/com/sda/she_likes_java/FinalProject/entity/User.java
+++ src/com/sda/she_likes_java/FinalProject/entity/User.java
@@ -3,7 +3,24 @@ package com.sda.she_likes_java.FinalProject.entity;
 
 import java.time.LocalDate;
 
-public record User(String name, String surname, LocalDate dateOfBirth, String login, String password,
-                   LocalDate drivingLicenseIssueDate) {
+public record User(
+        Long id,
+        String name,
+        String surname,
+        LocalDate dateOfBirth,
+        String login,
+        String password,
+        LocalDate drivingLicenseIssueDate) {
 
 }
+
+
+
+
+
+
+
+
+
+
+
